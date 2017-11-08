457e9f9b47b102e468e4644369575ec84a015452
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 21:24:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(side_a, side_b, side_c)
+    @a = side_a
+    @b = side_b
+    @c = side_c
+  end
+
+  def kind
+    if @a + @b > 0 && @a + @c > 0 && @c + @b > 0
+      case @a, @b, @c
+        when @a == @b == @c
+          :equilateral
+        when @a == @b || @b == @c || @a == @c
+          :isosceles
+        else
+          :scalene
+        end 
+    else 
+      raise TriangleError
+    end
+  end
 end
+
+class TriangleError < StandardError
+
+end
+
+  
